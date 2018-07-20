58e376535ffd9b779bd134aa184b669a5af33525
yusufdalva <yusuf.dalva.1@gmail.com>
2018-07-20 17:39:04 +0300

    Unused pages in sidebar removed

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -28,26 +28,6 @@
         <md-icon>content_paste</md-icon>
         <p>Table list</p>
       </sidebar-link>
-      <sidebar-link to="/typography">
-        <md-icon>library_books</md-icon>
-        <p>Typography</p>
-      </sidebar-link>
-      <sidebar-link to="/icons">
-        <md-icon>bubble_chart</md-icon>
-        <p>Icons</p>
-      </sidebar-link>
-      <sidebar-link to="/maps">
-        <md-icon>location_on</md-icon>
-        <p>Maps</p>
-      </sidebar-link>
-      <sidebar-link to="/notifications">
-        <md-icon>notifications</md-icon>
-        <p>Notifications</p>
-      </sidebar-link>
-      <sidebar-link to="/upgrade" class="active-pro">
-        <md-icon>unarchive</md-icon>
-        <p>Upgrade to PRO</p>
-      </sidebar-link>
     </side-bar>
 
     <div class="main-panel">
